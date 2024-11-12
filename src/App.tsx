4c39a478d9a04d181e4b9c60542e75623038f130
Antonio <antonio@fluidkey.com>
2024-11-13 00:17:16 +0700

    feat: removed unnecessary core and cleared imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import theme from './ui/theme';
 import Dashboard from './ui/pages/Dashboard';
 import { MainProvider } from './context/MainContext';
-import Core from '@walletconnect/core';
 import WalletKitProvider from './provider/WalletKitProvider';
-
-const core = new Core({
-  projectId: process.env.REACT_APP_REOWN_PROJECT_ID
-})
 
 function App(): JSX.Element {
 
